90e6cb3c0ded6711bb15c94c921889db881cfb54
Zachary A Gillis <zachary.gillis@kohls.com>
2018-10-04 16:06:46 -0500

    Port to Env Variable

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 
 app.get('/', function(req, res) {
     res.send('Hello yeet! This is a Node webserver app using express.');
